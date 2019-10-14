1892820e51ac5bfb56e9278467ffba720e987874
ThomasLodato <telodato.16@est.ucab.edu.ve>
2019-10-13 20:08:01 -0400

    thomas-002: Añadidos atributos api_key a owner, call_center y cuidador

--- BD-SCRIPTS/create_script_DailyDogCare.sql
+++ BD-SCRIPTS/create_script_DailyDogCare.sql
@@ -62,6 +62,7 @@ cui_telefono varchar(50) not null,
 cui_capacidad_maxima integer not null,
 cui_tarifa_diaria real not null,
 cui_fk_lugar integer not null,
+cui_api_key varchar(100),
 Constraint pk_id_cuidador primary key (cui_id),
 Constraint fk_lugar_cuidador foreign key (cui_fk_lugar) references Lugar(lug_id)
 );
@@ -77,6 +78,7 @@ own_correo varchar(50) not null unique,
 own_foto_perfil bytea,
 own_telefono varchar(50) not null,
 own_fk_lugar integer not null,
+own_api_key varchar(100),
 Constraint pk_id_owner primary key (own_id),
 Constraint fk_lugar_owner foreign key (own_fk_lugar) references Lugar(lug_id)
 );
@@ -92,6 +94,7 @@ cal_correo varchar(50) not null unique,
 cal_foto_perfil bytea,
 cal_telefono varchar(50) not null,
 cal_tipo_usuario varchar(50) not null,
+cal_api_key varchar(100),
 cal_fk_lugar integer not null,
 Constraint pk_id_call_center primary key (cal_id),
 Constraint fk_lugar_call_center foreign key (cal_fk_lugar) references Lugar(lug_id),
