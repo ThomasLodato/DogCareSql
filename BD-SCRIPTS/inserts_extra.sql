86725cd44ab8b14d14fc87909c32a6f6b1c371b5
NathalyTsang <nktsang.16@est.ucab.edu.ve>
2019-10-25 21:41:39 -0700

    Inserts de extra y tarea
    Si se les ocurren más, son bienvenidos a agregarlos o decirme :)

---
+++ BD-SCRIPTS/inserts_extra.sql
@@ -0,0 +1,11 @@
+INSERT INTO Extra (ext_nombre) values 
+('Jornada de 8hrs extra en su casa'),
+('Jornada de 8hrs extra en mi casa'),
+('Caminata de 30 minutos'),
+('Sesión de fotos - cantidad:10'),
+('Baño'),
+('Corte de uñas'),
+('Limpieza de oídos'),
+('Cepillado de dientes'),
+('Paseos por la playa'),
+('Traslado a otro lugar de la ciudad');
